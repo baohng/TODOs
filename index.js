3452e2a857144cd24ea0142e07ce3a1c9d506c46
baohng <bao.hng221b@gmail.com>
2023-07-29 09:40:28 +0700

    complete basic flow of application

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
-//app.use(express.static("public"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 
 //GET HOMEPAGE
 app.get("/", (req, res) => {
   res.render('index.ejs');
+});
+
+//POST
+let todoList = [];
+app.post("/work", (req, res) => {
+  let userNote = req.body['inputNote'];
+  if (userNote != null) {
+    todoList.push(userNote);
+  }
+  res.render('index.ejs', {
+    notes : todoList,
+  });
 });
 
 app.listen(port, () => {
